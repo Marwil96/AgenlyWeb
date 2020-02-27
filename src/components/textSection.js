315c2dc5c4c606e8a56294094b765eb54b653e8a
Marwil96 <william_martinsson@hotmail.com>
2020-02-27 13:03:14 +0100

    added load in animation

--- src/components/textSection.js
+++ src/components/textSection.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 import Button from "./button"
 
-const TextSection = ({img, title, text, button, modifier, link}) => {
+const TextSection = ({img, title, text, button, modifier, link, loaded}) => {
   return (
-    <section className={`TextSection ${modifier}`}>
+    <section className={`TextSection ${modifier}`} style={loaded ? {transform: `translateY(${0}px)`, opacity:1} : {transform: `translateY(${100}px)`, opacity:0}}>
       <div className="TextSection__textContainer">
         <h3 className="TextSection__title">{title}</h3>
         <span className="TextSection__text">{text}</span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -17,43 +17,51 @@ import TenReasons from "../components/tenReasons"
 import ExampleSites from "../components/exampleSites"
 import ActionBar from "../components/actionBar"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Build a Website - Webbyrå – Website Builder – Agenly' description='With Agenly you tell us about your business and we handle the rest. We use a combination between conversational UI and AI to create the best website for you.' author='William Martinsson' />
+const IndexPage = () => {
 
-    <Introduction />
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    setLoaded(true)
+  }, [])
+  return (
+    <Layout>
+      <SEO title='Build a Website - Webbyrå – Website Builder – Agenly' description='With Agenly you tell us about your business and we handle the rest. We use a combination between conversational UI and AI to create the best website for you.' author='William Martinsson' />
 
-    <TextSection 
-      modifier='left' 
-      img={setupIMG} 
-      title={<span>Have a <span className='highlighted'>conversation</span> with us and we will create the best website for you.</span>}
-      text={"For all the non-designers out there. We know its hard to create a website even with the various website creators. That's why we have built a unique setup process that is conversational instead of visual so we can learn as much as possible about your business to create the best website for you."} 
-    />
+      <Introduction />
+
+      <TextSection 
+        loaded={loaded}
+        modifier='left' 
+        img={setupIMG} 
+        title={<span>Have a <span className='highlighted'>conversation</span> with us and we will create the best website for you.</span>}
+        text={"For all the non-designers out there. We know its hard to create a website even with the various website creators. That's why we have built a unique setup process that is conversational instead of visual so we can learn as much as possible about your business to create the best website for you."} 
+      />
 
 
-    <ProcessSection />
+      <ProcessSection />
 
-    <TextSection 
-      modifier='right' 
-      img={section1Img} 
-      title={<span>In a forever <span className='highlighted'>changing</span> landscape, your website needs to change with it.</span>}
-      text={'The competition to be seen to the customers online has only grown fiercer. We help you to be seen by optimizing your SEO and working iteratively with your website through A/B testing. And that without you lifting a finger.'} 
-    />
+      <TextSection 
+        modifier='right' 
+        img={section1Img} 
+        title={<span>In a forever <span className='highlighted'>changing</span> landscape, your website needs to change with it.</span>}
+        text={'The competition to be seen to the customers online has only grown fiercer. We help you to be seen by optimizing your SEO and working iteratively with your website through A/B testing. And that without you lifting a finger.'} 
+      />
 
-    <TenReasons />
+      <TenReasons />
 
-    <TextSection 
-      modifier='left' 
-      img={modulesIMG} 
-      title={<span>Different businesses have different <span className='highlighted'>needs.</span></span>}
-      text={'That’s why we have plenty of modules to choose from, everything from restaurant menus to employee sections.'} 
-      // button='See the Modules.'
-      // link='/modules'
-    />
+      <TextSection 
+        modifier='left' 
+        img={modulesIMG} 
+        title={<span>Different businesses have different <span className='highlighted'>needs.</span></span>}
+        text={'That’s why we have plenty of modules to choose from, everything from restaurant menus to employee sections.'} 
+        // button='See the Modules.'
+        // link='/modules'
+      />
 
-    <ExampleSites />
-    <ActionBar />
-  </Layout>
-)
+      <ExampleSites />
+      <ActionBar />
+    </Layout>
+  )
+}
 
 export default IndexPage
