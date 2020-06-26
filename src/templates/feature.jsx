d289ce088d11e41f974bfe6da769b2a149b36a98
Marwil96 <william_martinsson@hotmail.com>
2020-06-26 16:01:56 +0200

    Image_block

--- src/templates/feature.jsx
+++ src/templates/feature.jsx
@@ -9,6 +9,7 @@ import ModulesLibrary from '../components/modulesLibrary';
 import TitleBlock from '../components/titleblock';
 import MockupBlock from '../components/mockupBlock';
 import RedirectBlock from '../components/redirectBlock';
+import ImageBlock from '../components/ImageBlock';
 
 const Feature = ({ data }) => {
   const featureData = data.prismic.allFeaturess.edges[0].node;
@@ -27,6 +28,7 @@ const Feature = ({ data }) => {
             {component.type === "module_cards" ? <ModulesLibrary cards={component.fields} /> : null}
             {component.type === "title_block" ? <TitleBlock title={component.primary.feature_title_block[0].text} />: null}
             {component.type === "mockup_image_block" ? <MockupBlock mobile={component.primary.mockup_block_mobile.url} desktop={component.primary.mockup_block_desktop.url}/> : null}
+            {component.type === "image_block" ? <ImageBlock image={component.primary.imageSharp.childImageSharp.fluid}/> : null}
             {component.type === "redirect_block" ? <RedirectBlock title={component.primary.redirect_title[0].text} subtitle={component.primary.redirect_subtitle[0].text}/> : null}
           </div>
         ))}
@@ -69,6 +71,31 @@ export const query = graphql`
                   redirect_title
                 }
             }
+            ... on PRISMIC_FeaturesBodyImage_block {
+                type
+                label
+                primary {
+                  image
+                  imageSharp {
+                    childImageSharp {
+                      fluid(maxWidth: 1920, quality: 70) {
+                         base64
+                         tracedSVG
+                         aspectRatio
+                         src
+                         srcSet
+                         srcWebp
+                         srcSetWebp
+                         sizes
+                         originalImg
+                         originalName
+                         presentationWidth
+                         presentationHeight
+                       }
+                    }
+                  }
+                }
+              } 
               ... on PRISMIC_FeaturesBodyModule_cards {
               fields {
                 module_card_title
